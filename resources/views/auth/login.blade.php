8b4b47be3dfa7676ba138f0f03b706c7db17304f
WafiFz <wafi20002@mail.unpad.ac.id>
2023-05-12 19:26:59 +0700

    remember me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
                 @endif
 
                 <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                    <input id="remember_me" type="checkbox" class="text-indigo-600 border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember_me">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
