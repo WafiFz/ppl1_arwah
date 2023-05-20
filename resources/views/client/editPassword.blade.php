5069d656fda52ff8cb0b5f569b6c514ca7023ecc
rnggptr10 <ranggaputra103@gmail.com>
2023-05-20 22:56:43 +0700

    update

--- resources/views/client/editPassword.blade.php
+++ resources/views/client/editPassword.blade.php
@@ -1,6 +1,6 @@
 <x-member-layout title="Profile">
     <main class="grow">
-        {{-- @if(session()->has('error'))
+        {{-- @if (session()->has('error'))
             <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                 <strong class="font-bold">Terjadi kesalahan!</strong>
                 <span class="block sm:inline">Tidak memenuhi validasi.</span>
